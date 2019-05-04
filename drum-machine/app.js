e81bbde815b1b1d5fffa90506758f44afd2cb73b
Jinme Mirabal <mirabalj@gmail.com>
2019-05-04 12:35:31 -0500

    Created container and one element with React template

---
+++ drum-machine/app.js
@@ -0,0 +1,21 @@
+class Display extends React.Component {
+  render() {
+    return(
+      <div>Drum Machine!</div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return(
+      <div id="drum-machine">
+        <Display />
+      </div>
+    )    
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
